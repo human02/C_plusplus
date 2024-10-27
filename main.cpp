9724608fda52f63294974f4eef7f9ae8da7cb932
human02 <42168454+human02@users.noreply.github.com>
2024-10-26 22:03:35 -0700

    add comments

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
+/*
+This
+is
+a
+multiline
+comment
+*/
 #include <iostream>
 
+consteval int get_value()
+{
+    return 7;
+}
+
+// Main fn is the netry point of the program
 int main()
 {
-    std::cout << "Hello World" << std::endl;
+    constexpr int value = get_value();
+    std::cout << "Hello World, the vaule is " << value << std::endl;
+
+    // this tells O that the main was successfully run.
+    return 0;
 }
