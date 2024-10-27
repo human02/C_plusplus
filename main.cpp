9b6249471b8272d27c27478d7bf43046baef762f
human02 <42168454+human02@users.noreply.github.com>
2024-10-26 22:46:05 -0700

    add functions

--- main.cpp
+++ main.cpp
@@ -7,17 +7,21 @@ comment
 */
 #include <iostream>
 
-consteval int get_value()
+int addNumber(int a, int b)
 {
-    return 7;
+    return a + b;
 }
 
-// Main fn is the netry point of the program
+// Main fn is the entry point of the program
 int main()
 {
-    constexpr int value = get_value();
-    std::cout << "Hello World, the vaule is " << value << std::endl;
+    std::cout << "Hello World" << std::endl;
+    // it defines the default value of a variable
+    int a{3};
+    int b{7};
+    std::cout << "Addition results in = " << a + b << std::endl;
 
+    std::cout << "Addition results (using Fn) = " << addNumber(20, b) << std::endl;
     // this tells O that the main was successfully run.
     return 0;
 }
